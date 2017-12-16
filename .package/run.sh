8f7cd6171aa34a9a4a210f25fa09800466f90fe8
Matthew Jones <matthewjones@Matthews-MBP.home>
2017-12-16 23:47:05 +0000

    added run script

---
+++ .package/run.sh
@@ -0,0 +1,8 @@
+
+source ${HOME}/package/install_homebrew.sh
+source ${HOME}/package/install_packages.sh
+
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  chsh -s $(grep /zsh$ /etc/shells | tail -1)
+fi
